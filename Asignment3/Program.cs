72874ddcd540f006661bd0ad06a2a2d8c58a3b32
Tejaspatil0307 <rp1767407@gmail.com>
2025-02-27 19:08:30 +0530

    Update Program.cs

--- Asignment3/Program.cs
+++ Asignment3/Program.cs
@@ -11,15 +11,29 @@
             //c1.carDetails(1, "Toyota", "Legander", 2015, 550000);
             c1.Display();
             Console.WriteLine("**************");
+            #region Q2 Parmaterize Constructor
 
-            Car c2 = new Car(2, "Toyota", "Camery", 2018, 500000);
+            //Car c2 = new Car(2, "Toyota", "Camery", 2018, 500000);
 
-            c2.Display();
+            //c2.Display();
+            #endregion
 
-            Console.WriteLine("enter password");
+            #region Q3 Password
+            //Console.WriteLine("Enter password it must contain 6 chars,atlist 1uppercase,one digit:");
+            //String pass = Console.ReadLine();
 
-            
-       
+            //if (pass.Length >= 6 && pass.Any(char.IsUpper) && pass.Any(char.IsDigit))
+            //{
+            //    Console.WriteLine("Valid Password:");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid password:");
+            //}
+            #endregion
+
+
+
 
         }
     }
